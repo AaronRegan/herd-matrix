47488d14d6a6e93560710ca47e9b372f2a58ae90
aaronregan <aaronregan20@gmail.com>
2020-09-29 21:58:38 +0200

    Remove Full Paths

--- spread_run.sh
+++ spread_run.sh
@@ -9,21 +9,21 @@ while true
 do
 	echo "Running with 40%"
 
-	sudo python /home/pi/herd_matrix/runtext.py -t 40%
+	sudo python runtext.py -t 40%
 
-	sudo python /home/pi/herd_matrix/spreading-block.py --protected 40
+	sudo python spreading-block.py --protected 40
 
 	echo "Running with 55%"
 
-	sudo python /home/pi/herd_matrix/runtext.py -t 55%
+	sudo python runtext.py -t 55%
 
-	sudo python /home/pi/herd_matrix/spreading-block.py --protected 55
+	sudo python spreading-block.py --protected 55
 
 	echo "Running with 70%"
 
-	sudo python /home/pi/herd_matrix/runtext.py -t 70%
+	sudo python runtext.py -t 70%
 
-	sudo python /home/pi/herd_matrix/spreading-block.py --protected 70
+	sudo python spreading-block.py --protected 70
 
 	echo "Sequence Restart"
 done
